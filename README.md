df52fbb84b5386e3a99efac8973981f66ce5a8c1
Kaishu Miyao <34433615+kaishumiyao@users.noreply.github.com>
2020-03-10 10:45:56 -0700

    added first unit test

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 # HOW TO RUN
 
 ```$ elm reactor```
+
+## Test Cases
+Install elm-test
+```$ npm install elm-test -g```
+Run test cases found in tests folder
+```$ elm-test```
--- tests/Example.elm
+++ tests/Example.elm
@@ -7,4 +7,5 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+   test "two plus two equals four"
+        (\_ -> Expect.equal 4 (2 + 2))
