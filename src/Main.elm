939e5720084e3850ddd449e064194594dc7edaa1
Ryan Premi <rpremi@calpoly.edu>
2020-03-10 14:02:26 -0700

    basic Values and serializer

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,39 @@ module Main exposing (..)
 
 import Html exposing (text)
 
+type Value
+    = NumV { num :  Float}
+    | BoolV { b : Bool }
+    | StrV  {str : String}
+    | PrimV {f : List Value -> Value}
+    | NullV  {msg : String}
+
+numTest =  NumV { num = 30.0}
+
+serialize : Value -> String
+
+serialize val = 
+    case val of 
+        NumV n ->
+            String.fromFloat n.num
+        BoolV b ->
+            "boolean"
+        StrV s ->
+            s.str
+        NullV n ->
+            "null"
+        PrimV p ->
+            "#procedure"
+
+
+
+printF = 
+    serialize numTest
 
 main =
-  text "Hello!"
+    text printF
+
+
+
+
+
